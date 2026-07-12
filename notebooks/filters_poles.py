6057d9f31833c88259333efc2a28ae33c12de44d
pl <ptrk.lechner@gmail.com>
2026-07-12 17:39:25 +0200

    pole app notebook

--- notebooks/filters_poles.py
+++ notebooks/filters_poles.py
@@ -1,6 +1,7 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
+#     "lcapy==1.26",
 #     "marimo>=0.23.14",
 #     "matplotlib==3.11.0",
 #     "numpy==2.5.1",
@@ -24,23 +25,18 @@ def _():
 
 @app.cell
 def _():
-
     import numpy as np
     import matplotlib.pyplot as plt
     import scipy.signal as sig
     from wigglystuff import ChartPuck
-    sr = 44100
-    T = 20
-    N = int(T*sr)
-    t = np.arange(N)/sr
+    N = 100
     π = np.pi
-    return ChartPuck, np, plt, π
+    return ChartPuck, N, np, plt, π
 
 
 @app.cell
-def _(mo, tf_wi, widget):
-
-    mo.hstack([widget, tf_wi])
+def _(irplot, mo, widget):
+    mo.hstack([widget, irplot], gap=0., justify="start", align="center")
 
     return
 
@@ -57,7 +53,7 @@ def _(ChartPuck, mo, np, plt, π):
     ax.set_ylabel("Y")
     ax.axhline()
     ax.axvline()
-    ax.set_title("Drag the puck to select coordinates")
+    ax.set_title("Drag the puck to change pole!")
     ax.grid(True, alpha=0.3)
     puck = ChartPuck(fig, x=0, y=0)
     plt.close(fig)
@@ -69,21 +65,32 @@ def _(ChartPuck, mo, np, plt, π):
 
 
 @app.cell
-def _(mo, np, plt, widget, π):
+def _(N, mo, np, plt, widget, π):
     p = widget.x[0] + 1j*widget.y[0]
-    ω = np.linspace(0, π, 100)
+    ω = np.linspace(0, π, N)
     z = np.exp(1j*ω)
     H = lambda z: 1 / (1 -p*z**-1)
-    figtf, axtf = plt.subplots(figsize=(3, 3))
-    axtf.plot(abs(H(z)))
-    tf_wi = mo.ui.matplotlib(axtf)
-    return p, tf_wi
+    figtf, axtf = plt.subplots(1,2,figsize=(8, 3))
+    axtf[0].plot(ω,20*np.log10(abs(H(z)+1e-6)))
+    axtf[0].set_ylim([-6,30])
+    mag = mo.ui.matplotlib(axtf[0])
+
+    h = np.fft.irfft(H(z),N*2)
+    h = np.fft.fftshift(h)
+    ns = np.arange(N*2)-N
+    axtf[1].plot(ns, h)
+    axtf[1].set_title("Impulse Response, $h[n]$")
+    axtf[1].set_xlabel('$n$')
+    plt.grid(True,alpha=0.3)
+    plt.ylim([-1,1])
+    irplot = mo.ui.matplotlib(axtf[1])
+    return (irplot,)
 
 
 @app.cell
-def _(p):
+def _():
 
-    p
+    # p
     return
 
 
