72d07470f5680e7a75bcfa08ab1dc589a8de5157
pl <ptrk.lechner@gmail.com>
2026-07-12 15:45:51 +0200

    filter pole

--- notebooks/filters_poles.py
+++ notebooks/filters_poles.py
@@ -2,25 +2,37 @@
 # requires-python = ">=3.13"
 # dependencies = [
 #     "marimo>=0.23.14",
+#     "matplotlib==3.11.0",
+#     "numpy==2.5.1",
+#     "scipy==1.18.0",
+#     "wigglystuff==0.5.13",
 # ]
 # ///
+
 import marimo
 
-__generated_with = "0.23.13"
-app = marimo.App(width="columns")
-
-
-@app.cell(column=0)
-def _(mo, puck):
-    widget = mo.ui.anywidget(puck)
-
-    widget
-    return (widget,)
+__generated_with = "0.23.14"
+app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
     import marimo as mo
+
+    return (mo,)
+
+
+@app.cell
+def _(mo, tf_wi, widget):
+
+    mo.hstack([widget, tf_wi])
+
+    return
+
+
+@app.cell
+def _():
+
     import numpy as np
     import matplotlib.pyplot as plt
     import scipy.signal as sig
@@ -30,12 +42,12 @@ def _():
     N = int(T*sr)
     t = np.arange(N)/sr
     π = np.pi
-    return ChartPuck, mo, np, plt, π
+    return ChartPuck, np, plt, π
 
 
 @app.cell
-def _(ChartPuck, np, plt, π):
-    fig, ax = plt.subplots(figsize=(6, 6))
+def _(ChartPuck, mo, np, plt, π):
+    fig, ax = plt.subplots(figsize=(3, 3))
     ω_uc = np.linspace(0, 2*π,100)
     uc = np.exp(1j*ω_uc)
     ax.plot(np.real(uc), np.imag(uc), alpha=0.6)
@@ -49,7 +61,26 @@ def _(ChartPuck, np, plt, π):
     ax.grid(True, alpha=0.3)
     puck = ChartPuck(fig, x=0, y=0)
     plt.close(fig)
-    return (puck,)
+
+    widget = mo.ui.anywidget(puck)
+
+
+    return (widget,)
+
+
+@app.cell
+def _(mo, np, plt, widget, π):
+
+
+    # ax = gca()
+    p = widget.x[0] + 1j*widget.y[0]
+    ω = np.linspace(0, π, 100)
+    z = np.exp(1j*ω)
+    H = lambda z: 1 / (1 -p*z**-1)
+    figtf, axtf = plt.subplots(figsize=(3, 3))
+    axtf.plot(abs(H(z)))
+    tf_wi = mo.ui.matplotlib(axtf)
+    return p, tf_wi
 
 
 @app.cell
@@ -60,7 +91,14 @@ def _(p):
 
 
 @app.cell
-def _():
+def _(widget):
+    widget.x[0]
+    return
+
+
+@app.cell
+def _(widget):
+    widget
     return
 
 
@@ -72,16 +110,6 @@ def _():
 @app.cell
 def _():
     return
-
-
-@app.cell(column=1)
-def _(np, plt, widget, π):
-    p = widget.x[0] + 1j*widget.y[0]
-    ω = np.linspace(0, π, 100)
-    z = np.exp(1j*ω)
-    H = lambda z: 1 / (1 -p*z**-1)
-    plt.plot(abs(H(z)))
-    return (p,)
 
 
 if __name__ == "__main__":
